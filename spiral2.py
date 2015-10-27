b0f58e3b9622970ca84b6af684cf6d89505df7f8
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2015-10-27 11:18:42 -0400

    Truncate floats

--- spiral2.py
+++ spiral2.py
@@ -43,6 +43,14 @@ class PolarImageConverter(object):
             l = [polar[0],polar[1],value]
             self.polarList.append(l)
         self.polarList.sort()
+        for i in range(len(self.polarList)):
+            l = self.polarList[i]
+            r = np.around(l[0], decimals=1)
+            t = np.around(l[1], decimals=1)
+            v = np.around(l[2], decimals=0)
+            tmp = [r,t,v]
+            self.polarList[i] = tmp
+
 
     def savePolarCSV(self, filename):
         with open(filename, 'wb') as f:
