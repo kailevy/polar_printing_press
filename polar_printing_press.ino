149499f1bf3c6e85473b65eb62602d599303039d
kailevy <kai0levy@gmail.com>
2015-10-27 11:53:28 -0400

    Add starter code for stepper motor control

---
+++ polar_printing_press.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_StepperMotor *motor1 = AFMS.getStepper(200, 1);
+
+int numsteps = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  AFMS.begin();
+  motor1->setSpeed(10);  // 10 rpm   
+}
+
+void loop() {
+  motor1->step(100, FORWARD, SINGLE); 
+  numsteps += 1;
+  Serial.println(numsteps);
+}
