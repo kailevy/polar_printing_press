fd6cc49c08b766ca46eca5012924d5b521e9b582
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2015-10-30 12:29:04 -0400

    add serial reading

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Hi this is a README. 
+# POE Polar Printing Press Project
+
+This repository will hold our code for our polar printing press. Our printing press will rotate and actuate markers in order to draw an image. 
+
+## Python 
+
+The Python parts of the code will process an image and convert this to a binarized or trinarized image. The Python code will convert this image into a set of polar drawing coordinates for the Arduino to read. 
+
+## Arduino
+
+The Arduino code reads from a CSV and controls the motors to draw the image. 
--- polar_printing_press.ino
+++ polar_printing_press.ino
@@ -30,3 +30,70 @@ void penDown() {
 void moveFromPointToPoint(float startX, float startY, float endX, float endY) {
 	// do things to move pen from point to point
 }
+
+void readSerialCommand() {
+  // we can only get one byte at a time, so we have to fill a string, and then parse it
+  String serialString = "";
+  int count = 0;
+  bool done = false;
+  while (Serial.available() && !done) {
+    delay(3);  // delay is apparently necessary
+    if (Serial.available() > 0) {
+      char c = Serial.read();
+      serialString += c;
+      count++;
+      if (c=='\n') { // Don't overflow serial string
+        serialString[count] = '\0'; // null terminate
+        done = true;
+      }
+    }
+  }
+
+  
+  if (serialString.length() > 0) {
+    String color; // color is 0 for black, 1 for dark gray, 2 for light gray
+    String value;  
+    command = serialString.substring(0,5); // read first 5 chars
+    value = serialString.substring(6,9);
+
+    int v = value.toInt();
+
+    if (v==0 && value!="000") { // check if conversion was actually made
+      Serial.print("Invalid value ");
+      Serial.println(value);
+      serialString = "";
+      return;
+    }
+
+    if (command == "avgws") {
+        Serial.print("Set averageWheelSpeed to ");
+        Serial.println(v);
+        averageWheelSpeed = v;
+    } else if (command == "cpwsc") {
+        Serial.print("Set controlPerWheelSpeedChange to ");
+        Serial.println(v);
+        controlPerWheelSpeedChange = v;
+    } else if (command == "mxerr") {
+        Serial.print("Set maxError to ");
+        Serial.println(v);
+        maxError = v;
+    } else if (command == "setkp") {
+        Serial.print("Set kP to ");
+        Serial.println(v);
+        kP = v;
+    } else if (command == "setki") {
+        Serial.print("Set kI to ");
+        Serial.println(v);
+        kI = v;
+    } else if (command == "setkd") {
+        Serial.print("Set kD to ");
+        Serial.println(v);
+        kD = v;
+    } else {
+      Serial.print("Invalid command "); 
+      Serial.println(command);
+    }
+
+    serialString = "";
+  } 
+}
