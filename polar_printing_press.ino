29301010c5321e7f14691cc311ca8608d66be218
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2015-11-01 17:16:14 -0500

    Arduino reading serial

--- polar_printing_press.ino
+++ polar_printing_press.ino
@@ -53,47 +53,9 @@ void readSerialCommand() {
   if (serialString.length() > 0) {
     String color; // color is 0 for black, 1 for dark gray, 2 for light gray
     String value;  
-    command = serialString.substring(0,5); // read first 5 chars
+    color = serialString.substring(0,5); // read first 5 chars
     value = serialString.substring(6,9);
 
     int v = value.toInt();
-
-    if (v==0 && value!="000") { // check if conversion was actually made
-      Serial.print("Invalid value ");
-      Serial.println(value);
-      serialString = "";
-      return;
-    }
-
-    if (command == "avgws") {
-        Serial.print("Set averageWheelSpeed to ");
-        Serial.println(v);
-        averageWheelSpeed = v;
-    } else if (command == "cpwsc") {
-        Serial.print("Set controlPerWheelSpeedChange to ");
-        Serial.println(v);
-        controlPerWheelSpeedChange = v;
-    } else if (command == "mxerr") {
-        Serial.print("Set maxError to ");
-        Serial.println(v);
-        maxError = v;
-    } else if (command == "setkp") {
-        Serial.print("Set kP to ");
-        Serial.println(v);
-        kP = v;
-    } else if (command == "setki") {
-        Serial.print("Set kI to ");
-        Serial.println(v);
-        kI = v;
-    } else if (command == "setkd") {
-        Serial.print("Set kD to ");
-        Serial.println(v);
-        kD = v;
-    } else {
-      Serial.print("Invalid command "); 
-      Serial.println(command);
-    }
-
-    serialString = "";
   } 
 }
