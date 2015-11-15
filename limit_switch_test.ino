eacf8efe8f2df009942fca4f4347abdfdee032b6
kailevy <kai0levy@gmail.com>
2015-11-15 15:10:24 -0500

    Add limit switch test

---
+++ limit_switch_test.ino
@@ -0,0 +1,30 @@
+int inputPin = 9;
+
+int buttonState;
+int val;
+int presses = 0;
+
+long time = 0;
+long debounce = 200;
+
+void setup() {
+   pinMode(inputPin, INPUT);
+   digitalWrite(inputPin, HIGH);
+
+   Serial.begin(9600);
+   buttonState = digitalRead(inputPin); // store initial button state (should be high)
+}
+
+void loop() {
+   val = digitalRead(inputPin);
+
+   if (val != buttonState && millis() - time > debounce) {
+     if (buttonState == HIGH){
+         presses = presses + 1;
+         Serial.println(presses);
+         time = millis();
+     }
+   }
+
+   buttonState = val;
+}
