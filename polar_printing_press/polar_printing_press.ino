c8a6b0d93db774fd6d64faf19071329615bf836a
kailevy <kai0levy@gmail.com>
2015-11-20 01:45:47 -0500

    Add late night code

--- polar_printing_press/polar_printing_press.ino
+++ polar_printing_press/polar_printing_press.ino
@@ -8,13 +8,15 @@ const boolean IN = true;
 const boolean OUT = !IN;
 
 // pins
-const int motorLimInputPin = 9; //input pin for limit switch
+const int motorLimInputPin = 8; //input pin for limit switch
 const int enablePin= 6;//Enable value of stepper motor. if it's not low it won't work
 const int stepPin = 5;
 const int dirPin = 4; //high is ?clockwise? and low is ?counter?
 const int markerPins[NUM_MARKER_PINS] = {9}; // the pins that the marker solenoids are on
 const int rotaryEncoderA = 12;  // pin 12
 const int rotaryEncoderB = 11;  // pin 11
+
+#define BAUD (9600) //Define the serial communication
 
 // current state variables
 int x; //variable for stepper driver looping
@@ -24,45 +26,48 @@ unsigned char encoderA;
 unsigned char encoderB;
 unsigned char encoderAprev=0;
 
+byte readyForCommand = 1;
+unsigned long currentAngle = 0;
+boolean forwardMotor= 1;
+int numRotations = 0;
+int rotationSteps = 0;
+
 // limit switch vars
 int buttonState; //state for limit switch
 int val; //val for limit switch
-int presses = 0; //times limit switch is pressed
 long time = 0; // time for limit switch
 long debounce = 200; //debounce for limit switch
 
-#define BAUD (9600) //Define the serial communication
+int stateMachine = 0;
+int halfCircle = 20*60; // calibrate this!
+// 1: alternate, 2: down, 3 & 4: up, increments every half circle
+boolean penState = 0;
 
-int numsteps = 0;
-byte readyForCommand = 1;
-unsigned long currentAngle = 0;
-bool forwardMotor= 1;
 
 void setup() {
   Serial.begin(BAUD);
 
-  // setup solenoids
-//  for (int i=0;i<NUM_MARKER_PINS;i++) {
-//    Serial.println(markerPins[i]);
-//    pinMode(markerPins[i], OUTPUT);
-//  }
+// setup solenoids
+  for (int i=0;i<NUM_MARKER_PINS;i++) {
+    Serial.println(markerPins[i]);
+    pinMode(markerPins[i], OUTPUT);
+  }
 
   // setup the stepper motor driver
   pinMode(enablePin, OUTPUT); //Enable value of stepper motor. if it's not low it won't work
   pinMode(stepPin, OUTPUT); // Step of stepper motor
   pinMode(dirPin,OUTPUT); //Dir of stepper motor. high is ?clockwise? and low is ?counter?
   digitalWrite(enablePin,LOW);//set Enable of stepper low
-//
-//  // move pens to beginning for setup
+
+  // move pens to beginning for setup
   moveToSide(motorLimInputPin, IN); // center the pens
-//
-//  // setup rotary encoder
-//  pinMode(rotaryEncoderA, INPUT);
-//  pinMode(rotaryEncoderB, INPUT);
-//  currentTime = millis();
-//  loopTime = currentTime;
-//
-//  // setup limit switch
+
+  // setup rotary encoder
+  pinMode(rotaryEncoderA, INPUT);
+  pinMode(rotaryEncoderB, INPUT);
+  currentTime = millis();
+  loopTime = currentTime;
+  // setup limit switch
   pinMode(motorLimInputPin, INPUT);
   digitalWrite(motorLimInputPin, HIGH);
   buttonState = digitalRead(motorLimInputPin); // store initial button state (should be high)
@@ -71,54 +76,38 @@ void setup() {
 void loop() {
   // read the encoder, this will update current state values
 
+  readRotationSwitch();
+  readEncoder();
+  Serial.println(rotationSteps);
+  if (rotationSteps >= halfCircle){
+    stateMachine = (stateMachine + 1)%4;
+    rotationSteps = 0;
+  }
+  switch(stateMachine){
+    case 0:
+      if ((rotationSteps / 20) % 2 == 1){
+        actuatePen(0,!penState);
+      }
+      break;
+    case 1:
+      actuatePen(0,1);
+      break;
+    default:
+      actuatePen(0,0);
+      break;
+  }
+        
+  // step the motor
+  stepMotor(true, 1);
+  
 
-//  readEncoder();
-//
-//  // step the motor
-//  stepMotor(true, 1);
-//
-//  // pen down, wait, pen up
-//  penDown(0);
-//  delay(500);
-//  penUp(0);
-//  delay(500);
-//
-//  // do a bunch of limit switch things
-//  val = digitalRead(motorLimInputPin);
-//
-//  //debounce the limit switch first
-//  if (val != buttonState && millis() - time > debounce) {
-//    if (buttonState == HIGH){
-//      presses = presses + 1;
-//      Serial.println(presses);
-//      time = millis();
-//
-//      //and have stepper spin in the opposite direction
-//
-//     }
-//   }
-//
-//   //spin stepper motor
-//  digitalWrite(EnablePin,LOW); // Set Enable low
-//  digitalWrite(DirPin,HIGH); // Set Dir high
-//  Serial.println("Loop 200 steps (1 rev)");
-//  for(x = 0; x < 200; x++) // Loop 200 times
-//  {
-//    digitalWrite(5,HIGH); // Output high
-//    delay(10); // Wait
-//    digitalWrite(StepPin,LOW); // Output low
-//    delay(100); // Wait
-//
-//    }
-//
-//  buttonState = val;
 }
 
-void stepMotor(bool clockwise, int steps) {
+void stepMotor(boolean clockwise, int steps) {
   if (clockwise) {
     digitalWrite(dirPin,HIGH); // Set Dir high
   } else {
-    digitalWrite(dirPin,LOW); // Set Dir high
+    digitalWrite(dirPin,LOW); // Set Dir low
   }
   for (int i=0; i<steps; i++) {
     Serial.println("Step");
@@ -127,6 +116,20 @@ void stepMotor(bool clockwise, int steps) {
     digitalWrite(stepPin,LOW); // Output low
     delay(1); // Wait
   }
+}
+
+void readRotationSwitch(){
+  // do a bunch of limit switch things
+  int val = digitalRead(motorLimInputPin);
+
+  //debounce the limit switch first
+  if (val != buttonState && millis() - time > debounce) {
+    if (buttonState == HIGH){
+      numRotations++;
+      time = millis();
+     }
+     buttonState = val;
+   }
 }
 
 void moveToSide(int inputPin, bool direction) {
@@ -148,15 +151,21 @@ void readEncoder() {
       // A has gone from high to low
       if(encoderB) {
         // B is high so clockwise
-        // TODO: Update current value
+        rotationSteps++;
       } else {
         // B is low so counter-clockwise
-        // TODO: Update current value
+        rotationSteps--;
       }
     }
     encoderAprev = encoderA;     // Store value of A for next time
     loopTime = currentTime;  // Updates loopTime
   }
+}
+
+void actuatePen(int marker, int state){
+  // state is either 0 or 1
+  digitalWrite(markerPins[marker], state);
+  penState = state;
 }
 
 void penDown(int marker) {
