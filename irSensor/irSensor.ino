e34c25b04a689574cb0a910157c515a683515e31
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2015-11-30 19:30:20 -0500

    Added infrared sensor code

---
+++ irSensor/irSensor.ino
@@ -0,0 +1,36 @@
+const int SENSOR_PIN = A5;
+const long DEBOUNCE_TIME = 10;
+const int SENSOR_THRESHOLD = 100;
+
+int numSteps = 0;
+int sensorValue = 0;
+int sensorState = 0; // 0 for black, 1 for white
+int prevSensorState = 0;
+long sensorLastDebounceTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  sensorLastDebounceTime = millis();
+}
+
+void loop() {
+  sensorValue = analogRead(SENSOR_PIN);
+
+  //Serial.println(sensorValue);
+
+  int currentState;
+  if (sensorValue >= SENSOR_THRESHOLD) {
+    currentState = 1;
+  } else {
+    currentState = 0;
+  }
+
+  if (currentState != sensorState) {
+    sensorState = currentState;
+    if (sensorState == 0) {
+      numSteps++;
+      Serial.println(numSteps);
+    }
+  }
+}
